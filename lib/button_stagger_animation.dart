ceb307a24e057ae6db1592998638a3df9aea5507
Pascal Brosinski <pascal@brosinski.eu>
2019-02-23 11:55:04 +0100

    Get size from widgets constraints instead of parameter

--- lib/button_stagger_animation.dart
+++ lib/button_stagger_animation.dart
@@ -3,12 +3,8 @@ import 'package:flutter/material.dart';
 class ButtonStaggerAnimation extends StatelessWidget {
   // Animation fields
   final AnimationController controller;
-  final Animation<double> widthAnimation;
-  final Animation<BorderRadius> borderRadiusAnimation;
 
   // Display fields
-  final double height;
-  final double width;
   final Color color;
   final Color progressIndicatorColor;
   final double progressIndicatorSize;
@@ -19,58 +15,20 @@ class ButtonStaggerAnimation extends StatelessWidget {
   ButtonStaggerAnimation({
     Key key,
     this.controller,
-    this.height,
-    this.width,
     this.color,
     this.progressIndicatorColor,
     this.progressIndicatorSize,
     this.borderRadius,
     this.onPressed,
     this.child,
-  })  : widthAnimation = Tween<double>(
-          begin: width,
-          end: height,
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Curves.easeInOut,
-          ),
-        ),
-        borderRadiusAnimation = Tween<BorderRadius>(
-          begin: borderRadius,
-          end: const BorderRadius.all(Radius.circular(30)),
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Curves.easeInOut,
-          ),
-        ),
-        super(key: key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (BuildContext context, Widget child) {
-        return Container(
-          width: widthAnimation.value,
-          height: height,
-          child: RaisedButton(
-            shape: RoundedRectangleBorder(
-              borderRadius: borderRadiusAnimation.value,
-            ),
-            color: color,
-            child: buttonChild(),
-            onPressed: () {
-              this.onPressed(controller);
-            },
-          ),
-        );
-      },
-    );
+    return LayoutBuilder(builder: _progressAnimatedBuilder);
   }
 
-  Widget buttonChild() {
+  Widget _buttonChild() {
     if (controller.isAnimating) {
       return Container();
     } else if (controller.isCompleted) {
@@ -83,5 +41,50 @@ class ButtonStaggerAnimation extends StatelessWidget {
       );
     }
     return child;
+  }
+
+  AnimatedBuilder _progressAnimatedBuilder(
+      BuildContext context, BoxConstraints constraints) {
+    var buttonHeight = (constraints.maxHeight != double.infinity)
+        ? constraints.maxHeight
+        : 60.0;
+
+    var widthAnimation = Tween<double>(
+      begin: constraints.maxWidth,
+      end: buttonHeight,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    var borderRadiusAnimation = Tween<BorderRadius>(
+      begin: borderRadius,
+      end: BorderRadius.all(Radius.circular(buttonHeight / 2.0)),
+    ).animate(CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeInOut,
+    ));
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        return SizedBox(
+          height: buttonHeight,
+          width: widthAnimation.value,
+          child: RaisedButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: borderRadiusAnimation.value,
+            ),
+            color: color,
+            child: _buttonChild(),
+            onPressed: () {
+              this.onPressed(controller);
+            },
+          ),
+        );
+      },
+    );
   }
 }
