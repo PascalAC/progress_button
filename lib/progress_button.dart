268a47a499c70b49c451246be64f110bdc6fee0a
Amrit Kahlon <amrit@hockey-community.com>
2021-04-15 19:14:17 -0700

    Migrated to null safety and added outlined button support

--- lib/progress_button.dart
+++ lib/progress_button.dart
@@ -1,39 +1,54 @@
 library progress_button;
 
 import 'package:flutter/material.dart';
-import 'package:progress_indicator_button/button_stagger_animation.dart';
+
+import './button_stagger_animation.dart';
 
 class ProgressButton extends StatefulWidget {
   /// The background color of the button.
   final Color color;
+
   /// The progress indicator color.
   final Color progressIndicatorColor;
+
   /// The size of the progress indicator.
   final double progressIndicatorSize;
+
   /// The border radius while NOT animating.
   final BorderRadius borderRadius;
+
   /// The duration of the animation.
   final Duration animationDuration;
 
   /// The stroke width of progress indicator.
   final double strokeWidth;
+
+  /// Changes to an outlined button.
+  final bool outlined;
+
+  /// The height of the button unless it is constrained.
+  final double height;
+
   /// Function that will be called at the on pressed event.
   ///
   /// This will grant access to its [AnimationController] so
   /// that the animation can be controlled based on the need.
   final Function(AnimationController) onPressed;
+
   /// The child to display on the button.
   final Widget child;
 
   ProgressButton({
-    @required this.child,
-    @required this.onPressed,
+    required this.child,
+    required this.onPressed,
     this.color = Colors.blue,
     this.strokeWidth = 2,
     this.progressIndicatorColor = Colors.white,
     this.progressIndicatorSize = 30,
     this.borderRadius = const BorderRadius.all(Radius.circular(0)),
     this.animationDuration = const Duration(milliseconds: 300),
+    this.outlined = false,
+    this.height = 60.0,
   });
 
   @override
@@ -42,7 +57,7 @@ class ProgressButton extends StatefulWidget {
 
 class _ProgressButtonState extends State<ProgressButton>
     with TickerProviderStateMixin {
-  AnimationController _controller;
+  AnimationController? _controller;
 
   @override
   void initState() {
@@ -56,7 +71,7 @@ class _ProgressButtonState extends State<ProgressButton>
 
   @override
   void dispose() {
-    _controller.dispose();
+    _controller?.dispose();
     super.dispose();
   }
 
@@ -64,13 +79,15 @@ class _ProgressButtonState extends State<ProgressButton>
   Widget build(BuildContext context) {
     return Center(
       child: ButtonStaggerAnimation(
-        controller: _controller.view,
+        controller: _controller!,
         color: widget.color,
         strokeWidth: widget.strokeWidth,
         progressIndicatorColor: widget.progressIndicatorColor,
         progressIndicatorSize: widget.progressIndicatorSize,
         borderRadius: widget.borderRadius,
         onPressed: widget.onPressed,
+        outlined: widget.outlined,
+        height: widget.height,
         child: widget.child,
       ),
     );
